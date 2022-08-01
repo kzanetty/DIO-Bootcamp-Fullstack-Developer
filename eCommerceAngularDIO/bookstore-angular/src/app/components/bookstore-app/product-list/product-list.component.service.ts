4eda70da49123bda1e2af7dd224d58dff2a25f1d
Henrique Zanetti <hzanettik12@gmail.com>
2022-07-31 22:17:58 -0300

    Add project BookStore Angular

---
+++ eCommerceAngularDIO/bookstore-angular/src/app/components/bookstore-app/product-list/product-list.component.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http"
+import { Book } from "./model/Book";
+
+@Injectable()
+export class BookService{
+  private url = 'https://localhost:44382/api/bookstore'
+
+  httpOptions = {
+    Headers : new HttpHeaders({'content-type':'application/json'})
+  }
+
+  constructor(private httpClient: HttpClient){}
+
+  getBook(){
+    return this.httpClient.get(this.url)
+  }
+}
